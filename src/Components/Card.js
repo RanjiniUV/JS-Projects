a2433c1abc6d1f024bf958ae5ec89f44dd1ded6d
RanjiniUV <Ranjinirao91@gmail.com>
2020-03-23 18:12:17 -0700

    CSS clean up

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import PropTypes from 'prop-types';
 import {render} from 'react-dom';
-import cardContainer from '../Styles/custom.css';
+import '../Styles/custom.css';
 
 class Card extends Component {
     render() {
@@ -11,19 +11,14 @@ class Card extends Component {
     
   <article className="card" key={cards.name}>
     <picture className="thumbnail">
-         <img className="category__01" src="https://abbeyjfitzgerald.com/wp-content/uploads/2018/01/cloud.svg" alt="" />
+         <img className="category__01" src={cards.imageUrl} alt="" />
     </picture>
     <div className="card-content">
-      <p className="category category__01">Cloud Storage</p>
-      <h2>Photo Storage</h2>
-      <p>TUX re-inventing the wheel, and move the needle. Feature creep dogpile that but diversify kpis but market-facing.</p>
+      <h2>{cards.name}</h2>
+    <p>Text : {cards.text}</p>
+    <p>Set Name : {cards.set.name}</p>
+    <p>Type : {cards.type}</p>
    </div>
-    <footer>
-      <div className="post-meta">
-        <span className="timestamp"><i className="fa fa-clock-o"></i> 6 mins ago</span>
-        <span className="comments"><i className="fa fa-comments"></i><a href="#"> 14 comments</a></span>
-        </div>
-    </footer>
   </article>
               
             
