32cc7fafee09031b2afaebd55f01419dbf7675b5
Ranjini Uma Vishwanath <38837085+RanjiniUV@users.noreply.github.com>
2026-03-10 16:12:22 -0700

    Update SearchBar.js

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,7 +1,8 @@
 import React,{Component} from 'react';
 import {render} from 'react-dom';
 import PropTypes from 'prop-types';
-import  '../Styles/searchBar.scss';
+//import  '../Styles/searchBar.scss';
+import 'src/Styles/searchBar.scss';
 
 class SearchBar extends React.Component {
     constructor(props){
